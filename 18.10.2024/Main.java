b89467dc531aa30c89e2ff1e7c76f0e07abc9502
Avto Ukhurgunashvili <avtouxu@gmail.com>
2024-10-23 18:35:43 +0400

    Add files via upload

---
+++ 18.10.2024/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+        public static void main(String[] args) {
+            //1
+//            int C = 24  ;
+//            float F = (C * 9.0F / 5 ) +32 ;
+//            System.out.println("temperature in fahrenheit is " + F );
+
+            //2
+//            float F = 79F ;
+//
+//            float C = 5/9F * (F - 32 );
+//
+//            System.out.println("temperature in celsius is " + C);
+
+            //3
+//            int Year = 1900;
+//
+//            if(Year% 400 == 0 ) {
+//                System.out.println("Nakiania :) ");
+//            }
+//                else if(Year %4 == 0 && Year%100 ==0 ){
+//                System.out.println("Nakiani ar aris :( ");
+//            }
+
+              //4
+//            int x = -2 ;
+//            int y = 5 ;
+//            if (x>0 && y>0){
+//                System.out.println("wertili pirvel meotxedshia");
+//            }
+//            else if (x<0 && y>0 ){
+//                System.out.println("wertili meore meotxedshia");
+//            }
+//            else if(x<0 && y<0){
+//                System.out.println("wertili mesame meotxedshia");
+//            }
+//            else{
+//                System.out.println("wertili meotxe meotxedshia");
+//            }
+
+            //5
+
+            int a = 15 ;
+            int b = 5 ;
+
+            String x = "/" ;
+
+            if(x == "/" ){
+                System.out.println(a/b);
+            }
+            else if(x == "*" ){
+                System.out.println(a*b);
+            }
+            else if(x == "-" ){
+                System.out.println(a-b);
+            }
+            else {
+                System.out.println(a+b);
+            }
+
+        }
+}
